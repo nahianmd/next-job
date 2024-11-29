5531be94bac32194b7eb2db0fc4ecd8519e67834
Md. Nahian <md.nahian36@gmail.com>
2024-11-29 09:32:02 +0600

    taskId: source: cause: solution: Authguard omitted

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,13 @@
 // PROJECT IMPORTS
 import MainLayout from '../../layouts/MainLayout';
 import React from 'react';
-import AuthGuard from '@/utils/route-guard/AuthGuard';
 
 // ================================|| MAIN LAYOUT ||================================ //
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthGuard>
-      <MainLayout>{children}</MainLayout>
-    </AuthGuard>
+    // <AuthGuard>
+    <MainLayout>{children}</MainLayout>
+    // </AuthGuard>
   );
 }
